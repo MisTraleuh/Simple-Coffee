612e53e30d279dde2b9e68117fb75bfb9884f114
Nathan <nathan.catalan@epitech.eu>
2025-02-16 01:53:22 +0100

    [ADD] details product provider

---
+++ lib/shared/providers/pages/details_product_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePageModel extends ChangeNotifier {
+  String selectedButton = "M";
+  int _selectedIndex = 0;
+
+  int get selectedIndex => _selectedIndex;
+
+  void setSelectedButton(String button) {
+    selectedButton = button;
+    notifyListeners();
+  }
+
+  void setSelectedIndex(int index) {
+    _selectedIndex = index;
+    notifyListeners();
+  }
+}
